d2d4a412c98de0215b5e511f0de21929bcaaf33c
MahdiAghaali <mahdi.aghaali@yahoo.com>
2022-11-11 02:01:36 +0330

    Use redux in book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,46 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 
-class Book extends React.PureComponent {
-  render() {
-    const { title, author } = this.props;
-    return (
-      <div className="book">
-        <div className="bookInfo">
-          <h2 className="bookTitle">{title}</h2>
-          <span className="bookAuthor">{author}</span>
-        </div>
-        <div className="bookActions">
-          <button type="button">Comments</button>
-          <button type="button">Remove</button>
-          <button type="button">Edit</button>
-        </div>
-        <div className="progressBlock">
-          <progress value="32" max="100"> 32% </progress>
-          <span>32%</span>
-        </div>
-        <div className="chapterInfo">
-          <div>
-            chapter info
-          </div>
-          <div>
-            introduction
-          </div>
-          <button type="button">
-            Update progress
-          </button>
-        </div>
+const Book = (props) => {
+  const { id, title, author } = props;
+  const dispatch = useDispatch();
+  return (
+    <div className="book" id={id}>
+      <div className="bookInfo">
+        <h2 className="bookTitle">{title}</h2>
+        <span className="bookAuthor">{author}</span>
       </div>
-    );
-  }
-}
+      <div className="bookActions">
+        <button type="button">Comments</button>
+        <button type="button" onClick={() => { dispatch(removeBook(id)); }}>Remove</button>
+        <button type="button">Edit</button>
+      </div>
+      <div className="progressBlock">
+        <progress value="32" max="100"> 32% </progress>
+        <span>32%</span>
+      </div>
+      <div className="chapterInfo">
+        <div>
+          chapter info
+        </div>
+        <div>
+          introduction
+        </div>
+        <button type="button">
+          Update progress
+        </button>
+      </div>
+    </div>
+  );
+};
 
 Book.defaultProps = {
+  id: '0',
   title: 'Title',
   author: 'Author',
+
 };
 
 Book.propTypes = {
+  id: PropTypes.string,
   title: PropTypes.string,
   author: PropTypes.string,
 };
