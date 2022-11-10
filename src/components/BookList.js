ef0a09806719b94c3a1eb38e27b58a0cd26dc4ed
MahdiAghaali <mahdi.aghaali@yahoo.com>
2022-11-11 02:01:50 +0330

    Add redux in booklist component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Book from './Book';
 
-class BookList extends React.PureComponent {
-  render() {
-    const bookArray = [
-      { key: 1, title: 'myBook', author: 'me' },
-      { key: 2, title: 'yourBook', author: 'you' },
-      { key: 3, title: 'ourBook', author: 'us' },
-    ];
-    return (
-      <ul>
-        {bookArray.map((book) => <Book key={book.key} title={book.title} author={book.author} />)}
-      </ul>
-    );
-  }
-}
+const BookList = () => {
+  const bookArray = useSelector((state) => state.bookReducer);
+
+  return (
+    <ul>
+      {bookArray.map((book) => (
+        <Book
+          key={book.key}
+          id={book.key}
+          title={book.title}
+          author={book.author}
+        />
+      ))}
+    </ul>
+  );
+};
 
 export default BookList;
