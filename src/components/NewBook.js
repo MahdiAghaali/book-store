87302aed7d4e2e47a2ebd6c4691286533fd051f1
MahdiAghaali <mahdi.aghaali@yahoo.com>
2022-11-11 02:02:19 +0330

    Add redux in new book component

--- src/components/NewBook.js
+++ src/components/NewBook.js
@@ -1,18 +1,32 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../redux/books/books';
 
-class NewBook extends React.PureComponent {
-  render() {
-    return (
-      <form>
-        <h2>Add new Book</h2>
-        <span>title:</span>
-        <input id="bookTitleInput" />
-        <span>author:</span>
-        <input id="bookAuthorInput" />
-        <button type="button">Add Book</button>
-      </form>
-    );
-  }
-}
-
+const NewBook = () => {
+  const dispatch = useDispatch();
+  return (
+    <form>
+      <h2>Add new Book</h2>
+      <span>title:</span>
+      <input id="bookTitleInput" required />
+      <span>author:</span>
+      <input id="bookAuthorInput" required />
+      <button
+        type="button"
+        onClick={() => {
+          const title = document.getElementById('bookTitleInput').value;
+          const author = document.getElementById('bookAuthorInput').value;
+          if (title !== '' && author !== '') {
+            const key = uuidv4();
+            const book = { key, title, author };
+            dispatch(addBook(book));
+          }
+        }}
+      >
+        Add Book
+      </button>
+    </form>
+  );
+};
 export default NewBook;
