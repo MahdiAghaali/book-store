74423dfb82a5c6f82982505e1ea816dbf527997b
Mahdi Aghaali <Mahdi.aghaali@yahoo.com>
2022-11-11 23:15:14 +0330

    Use async function to get data from API

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Book from './Book';
+import { getBookAsync } from '../redux/books/books';
 
 const BookList = () => {
   const bookArray = useSelector((state) => state.bookReducer);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBookAsync());
+  }, []);
 
   return (
     <ul>
       {bookArray.map((book) => (
         <Book
-          key={book.key}
-          id={book.key}
+          key={book.item_id}
+          id={book.item_id}
           title={book.title}
           author={book.author}
         />
