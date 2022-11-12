a94c178625f9a90b8147dbbc418e15b34886d1a8
Mahdi Aghaali <Mahdi.aghaali@yahoo.com>
2022-11-12 20:50:38 +0330

    Add progress bar to books component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,36 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
+import { CircularProgressbar } from 'react-circular-progressbar';
 import { removeBookAsync } from '../redux/books/books';
+import 'react-circular-progressbar/dist/styles.css';
 
 const Book = (props) => {
-  const { id, title, author } = props;
+  const {
+    id, title, author, category,
+  } = props;
   const dispatch = useDispatch();
+  const progress = parseInt(Math.random() * 100, 10);
   return (
     <div className="book" id={id}>
       <div className="bookInfo">
-        <h2 className="bookTitle">{title}</h2>
+        <span className="bookCategory">{category}</span>
+        <span className="bookTitle">{title}</span>
         <span className="bookAuthor">{author}</span>
-      </div>
-      <div className="bookActions">
-        <button type="button">Comments</button>
-        <button type="button" onClick={() => { dispatch(removeBookAsync(id)); }}>Remove</button>
-        <button type="button">Edit</button>
-      </div>
-      <div className="progressBlock">
-        <progress value="32" max="100"> 32% </progress>
-        <span>32%</span>
-      </div>
-      <div className="chapterInfo">
-        <div>
-          chapter info
+        <div className="bookActions">
+          <button type="button">Comments</button>
+          <div className="verticalLine" />
+          <button type="button" onClick={() => { dispatch(removeBookAsync(id)); }}>Remove</button>
+          <div className="verticalLine" />
+          <button type="button">Edit</button>
         </div>
-        <div>
-          introduction
+      </div>
+      <div className="infoContainer">
+        <div className="progressContainer">
+          <CircularProgressbar value={progress} text={`${progress}%`} />
         </div>
-        <button type="button">
-          Update progress
-        </button>
+        <div className="verticalLine big" />
+        <div className="chapterInfo">
+          <div className="currenChapter">
+            chapter info
+          </div>
+          <div>
+            introduction
+          </div>
+          <button type="button">
+            Update progress
+          </button>
+        </div>
       </div>
     </div>
   );
@@ -40,13 +50,14 @@ Book.defaultProps = {
   id: '0',
   title: 'Title',
   author: 'Author',
-
+  category: 'Action',
 };
 
 Book.propTypes = {
   id: PropTypes.string,
   title: PropTypes.string,
   author: PropTypes.string,
+  category: PropTypes.string,
 };
 
 export default Book;
